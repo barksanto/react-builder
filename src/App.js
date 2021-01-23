194fc31ee5c37735801467d40a275265eaa860a5
Barkley Santo <barkley.santo@gmail.com>
2021-01-23 12:08:37 -0500

    first component - playing around with React.createElement

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Welcome to the show</h1>
+    //     </header>
+    //     <p className="App-intro">
+    //       To get started, edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //   </div>
+    // );
+    return React.createElement('div', null, React.createElement('h1', null, "Does this work now?"))
+
   }
 }
 
