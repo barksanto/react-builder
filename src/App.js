c1b8e63f91a7389d3c80e551b3a06b352c87a21e
Barkley Santo <barkley.santo@gmail.com>
2021-01-23 12:32:11 -0500

    import person component to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Person from './Person/Person'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>It's working !!!</p>
+        <Person />
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', { className: 'App' }, "Does this work now?"))
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const person = () => {
+  return <p>I'm the person component</p>
+}
+
+export default person;
