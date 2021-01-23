462ca1f562c26747a60fcef9443d4899d1245f66
Barkley Santo <barkley.santo@gmail.com>
2021-01-23 13:19:39 -0500

    using state to render information for component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Barkley', age: 27 },
+      { name: 'Mikayla', age: 20 },
+      { name: 'Mango', age: 6 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>It's working !!!</p>
-        <Person name="Barkley" age="27" />
-        <Person name="Mikayla" age="20">My Hobbies: Shoes</Person>
-        <Person name="Mango" age="6" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Shoes</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
