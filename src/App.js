d7a5ccc1da01373c404050df82238308cd9a6a06
Barkley Santo <barkley.santo@gmail.com>
2021-01-24 11:36:24 -0500

    changed from functional component to class based again so I can pass switchName handler by reference to children component as a prop

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-// import React, { Component } from 'react';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+// import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
-// class App extends Component
 
-const App = props => {
+class App extends Component {
+ state = {
+  persons: [
+    { name: 'Barkley', age: 27 },
+    { name: 'Mikayla', age: 20 },
+    { name: 'Mango', age: 6 }
+  ]
+}
 
-  const [personsState, setPersonsState] = useState({
+switchNameHandler = () => {
+  // console.log('was clicked!!!');
+  // new state! - it will merge the old state with the new one
+  this.setState({
     persons: [
-      { name: 'Barkley', age: 27 },
-      { name: 'Mikayla', age: 20 },
+      { name: 'Spongebob', age: 27 },
+      { name: 'Mikayla', age: 100 },
       { name: 'Mango', age: 6 }
-    ],
-    otherState: 'some other value'
-  });
+    ]
+  })
+}
 
-  const switchNameHandler = () => {
-    // console.log('was clicked!!!');
-    // new state! - it will merge the old state with the new one
-    setPersonsState({
-      persons: [
-        { name: 'Spongebob', age: 27 },
-        { name: 'Mikayla', age: 100 },
-        { name: 'Mango', age: 6 }
-      ]
-    });
-  };
-
+render() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>It's working !!!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
       <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age}
         click={this.switchNameHandler} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: Shoes</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Shoes</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
     </div>
   );
+  }
 }
 
 
@@ -64,3 +63,46 @@ export default App;
 //       { name: 'Mango', age: 6 }
 //     ]
 //   });
+
+
+
+// this code is functional component using hooks
+// const App = props => {
+  //   const [personsState, setPersonsState] = useState({
+  //     persons: [
+  //       { name: 'Barkley', age: 27 },
+  //       { name: 'Mikayla', age: 20 },
+  //       { name: 'Mango', age: 6 }
+  //     ],
+  //     otherState: 'some other value'
+  //   });
+  
+  //   const switchNameHandler = () => {
+  //     // console.log('was clicked!!!');
+  //     // new state! - it will merge the old state with the new one
+  //     setPersonsState({
+  //       persons: [
+  //         { name: 'Spongebob', age: 27 },
+  //         { name: 'Mikayla', age: 100 },
+  //         { name: 'Mango', age: 6 }
+  //       ]
+  //     });
+  //   };
+  
+  //   return (
+  //     <div className="App">
+  //       <h1>Hi, I'm a React App</h1>
+  //       <p>It's working !!!</p>
+  //       <button onClick={switchNameHandler}>Switch Name</button>
+  //       <Person
+  //         name={personsState.persons[0].name}
+  //         age={personsState.persons[0].age}
+  //         click={this.switchNameHandler} />
+  //       <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: Shoes</Person>
+  //       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+  //     </div>
+  //   );
+  // }
+  
+  
+  // export default App;
