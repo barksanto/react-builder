11ae21a8a7ebfa33891ef05686625fbb22b7ce54
Barkley Santo <barkley.santo@gmail.com>
2021-01-26 12:11:12 -0500

    add styling for each li component rendered after typing

--- src/CharComponent.js
+++ src/CharComponent.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const charComponent = (props) => {
-  // const style = {}
+  const style = {
+    display: 'inline-block',
+    padding: '16px',
+    textAlign: 'center',
+    margin: '16px',
+    border: '1px solid black'
+  }
 
   let charArray = [];
   if (props.chars) {
@@ -10,13 +16,15 @@ const charComponent = (props) => {
   }
   let eachThing = null
   if (charArray.length > 0) {
-    eachThing = charArray.map((element) => {
-      return <p>{element}</p>
+    eachThing = charArray.map((element, index) => {
+      return <li key={index} style={style}>{element}</li>
     })
   }
   return (
     <div>
-      {eachThing}
+      <ul>
+        {eachThing}
+      </ul>
     </div>
   )
 
