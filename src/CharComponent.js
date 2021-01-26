2a8ced14c220b72905f86f99f05c94591027a182
Barkley Santo <barkley.santo@gmail.com>
2021-01-26 12:08:03 -0500

    render each character of the string into its own component

--- src/CharComponent.js
+++ src/CharComponent.js
@@ -1,17 +1,25 @@
 import React from 'react'
 
 const charComponent = (props) => {
-  let charArray = null;
-  if (props.chars){
+  // const style = {}
+
+  let charArray = [];
+  if (props.chars) {
     charArray = props.chars.split('');
     console.log(charArray)
   }
-
+  let eachThing = null
+  if (charArray.length > 0) {
+    eachThing = charArray.map((element) => {
+      return <p>{element}</p>
+    })
+  }
   return (
     <div>
-      
+      {eachThing}
     </div>
   )
+
 }
 
 export default charComponent;
